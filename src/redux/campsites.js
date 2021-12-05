7e82210e47316ebb1d5988adf65514f9d036497a
nhungle <misssg85@gmail.com>
2021-12-05 07:05:54 -0800

    Combining Reducers

---
+++ src/redux/campsites.js
@@ -0,0 +1,8 @@
+import { CAMPSITES } from '../share/campsites';
+
+export const Campsites = (state = CAMPSITES, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
---
+++ src/redux/partners.js
@@ -0,0 +1,8 @@
+import { PARTNERS } from '../share/partners';
+
+export const Partners = (state = PARTNERS, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
